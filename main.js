9f1f898b7aa720039211f6ff0564a0232759304d
r21dm <rockydwimakmur@gmail.com>
2022-10-10 00:30:05 +0700

    for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit. e:  e !!!#

---
+++ main.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+
+const baseUrl = "https://api-pub.bitfinex.com/v2/";
+const pathParams = "trades/fUSD/hist";
+const queryParams = `limit=10000`;
+const period = 120;
+
+console.log(baseUrl + pathParams);
+
+axios.get(`${baseUrl}/${pathParams}?${queryParams}`).then(
+  (response) => {
+    // console.log(response.data);
+    let volume = 0;
+    response.data.forEach((x) => {
+      if (x[4] == period) {
+        console.log(
+          `${new Date(x[1]).toLocaleDateString()} ${new Date(
+            x[1]
+          ).toLocaleTimeString()}\t${Math.abs(x[2]).toFixed(4)}\t${(
+            x[3] * 100
+          ).toFixed(6)}%\t${x[4]}`
+        );
+        volume += Math.abs(x[2].toFixed(4));
+      }
+    });
+    console.log(`Total volume: ${volume}`);
+  },
+  (error) => {
+    console.log(error);
+  }
+);
