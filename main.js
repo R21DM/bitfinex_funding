3ef9ff1c01e1cb326ddd7f16b95e6f922ef10d00
r21dm <rockydwimakmur@gmail.com>
2023-02-16 23:15:26 +0700

    Fix sort by recent record

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const baseUrl = "https://api-pub.bitfinex.com/v2/";
 const pathParams = "trades/fUSD/hist";
-const queryParams = `limit=10000`;
+const queryParams = `limit=1000`;
 const period = 120;
 
 console.log(baseUrl + pathParams);
@@ -11,7 +11,7 @@ axios.get(`${baseUrl}/${pathParams}?${queryParams}`).then(
   (response) => {
     // console.log(response.data);
     let volume = 0;
-    response.data.forEach((x) => {
+    response.data.reverse().forEach((x) => {
       if (x[4] == period) {
         console.log(
           `${new Date(x[1]).toLocaleDateString()} ${new Date(
